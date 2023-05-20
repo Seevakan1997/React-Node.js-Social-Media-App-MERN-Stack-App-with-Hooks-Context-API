a54c8c4ec35e131b39954e74ae189c856d18f5cc
Seevakan1997 <seevakan1@gmail.com>
2023-05-20 19:29:39 +0530

    updated login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
 import './login.css'
+import { loginCall } from '../../apiCalls';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Login() {
+
+const email = useRef();
+const password = useRef();
+const {user,isFetching,error,dispatch} = useContext(AuthContext);
+
+const handleClick = (e)=>{
+  e.preventDefault();
+  loginCall({email:email.current.value,password:password.current.value},dispatch)
+}
+
+console.log(user);
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -12,15 +26,14 @@ export default function Login() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <button className="loginButton">Log In</button>
+          <form className="loginBox" onSubmit={handleClick}>
+            <input placeholder="Email" type="email" className="loginInput" ref={email} required />
+            <input placeholder="Password" type="password" className="loginInput" ref={password} minLength="6" required />
+            <button className="loginButton" type="submit" disabled={isFetching}>{isFetching ? "Loading..." : "Log In"}</button>
             <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegisterButton">
-              Create a New Account
+            <button className="loginRegisterButton">{isFetching ? "Loading..." : "Create a New Account"}
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
